e5cddc3c5e2f9702b5c50b9f7a711a9e0ebe6e20
TarasKo4444 <tarasko4444@gmail.com>
2023-08-02 11:26:18 +0300

    Removed unused validation

--- src/Core/CleanArchitecture.Application/Commands/Products/Queries/GetProduct/GetProductQueryValidator.cs
+++ src/Core/CleanArchitecture.Application/Commands/Products/Queries/GetProduct/GetProductQueryValidator.cs
@@ -7,6 +7,5 @@ public class GetProductQueryValidator : AbstractValidator<GetProductQuery>
     public GetProductQueryValidator()
     {
         RuleFor(p => p.Id)
-            .NotNull().WithMessage("Id not provided")
             .NotEqual(Guid.Empty).WithMessage("Id cannot be empty");    }
 }
